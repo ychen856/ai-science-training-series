191fa6adf759d489757cabc7e149c2123ee7c5ac
Yi-Chun Chen <ychen514@uic.edu>
2022-11-13 16:06:23 -0600

    add hw6 code

---
+++ 06_distributedTraining/homework/train_tensorflow2_mnist_thetagpu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#COBALT -t 20
+#COBALT -q training-gpu
+#COBALT -n 1
+#COBALT -A ALCFAITP
+#COBALT --attrs filesystems=home,grand
+
+# Set up software deps:
+module load conda/2022-07-01
+conda activate
+
+# You have to point this to YOUR local copy of ai-science-training-series
+
+export TF_XLA_FLAGS="--tf_xla_auto_jit=2"
+mpirun -np 1 python tensorflow2_mnist_hvd.py --num_steps 10
+mpirun -np 2 python tensorflow2_mnist_hvd.py --num_steps 10
+mpirun -np 4 python tensorflow2_mnist_hvd.py --num_steps 10
+mpirun -np 8 python tensorflow2_mnist_hvd.py --num_steps 10
+mpirun -np 16 python tensorflow2_mnist_hvd.py --num_steps 10
