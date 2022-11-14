cd1bb6cc885efe50e8c3be9aeddf107cbbe994af
Yi-Chun Chen <ychen514@uic.edu>
2022-11-14 00:43:19 -0600

    update hw7 file

---
+++ 07_largeScaleTraining/src/ai4sci/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#PBS -l select=1:system=polaris
+#PBS -l place=scatter
+#PBS -l walltime=1:00:00
+#PBS -q debug
+#PBS -A datascience
+#PBS -l filesystems=home:eagle
+
+
+# Set up software deps:
+module load conda/2022-09-08
+conda activate
